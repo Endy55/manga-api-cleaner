e330895221e901a1145a684b57f8c9721b6010b2
Endy55 <ezarate@willamette.edu>
2025-04-19 23:37:56 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-export PGHOST=tramway.proxy.rlwy.net
-export PGPORT=14325
+export PGHOST=postgres-azn1.railway.internal
+export PGPORT=5432
 export PGDATABASE=railway
 export PGUSER=postgres
-export PGPASSWORD=FUnmUbtdGlHtyCqguVfoTSlHNEIVCJkv
+export PGPASSWORD=MhpffpgLfLNJevKSbCdyDSbwgxCihUYN
 
 psql -f clean.sql
